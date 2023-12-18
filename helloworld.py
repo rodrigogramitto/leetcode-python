c2d17dc9a8b94fc93b11efe3e8401426930d8c2b
rodrigogramitto <rgramitto@gmail.com>
2023-12-18 16:04:20 -0500

    py hello

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+def main():
+    name = input('Hello Human, would you kindly tell me your name?');
+    print('Nice to meet you,', name, '!');
+    print();
+    print(type(name));
+
+if __name__ == '__main__':
+    main()
